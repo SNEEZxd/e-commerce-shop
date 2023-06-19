cdbe5c677af542dd17418679d63041489e54381c
SNEEZxd <88846720+SNEEZxd@users.noreply.github.com>
2023-06-20 00:04:03 +0200

    init commit

---
+++ backend/apps/catalogue/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "catalogue"
+
+urlpatterns = [
+    path("", views.product_all, name="catalogue_home"),
+    path("<slug:slug>", views.product_detail, name="product_detail"),
+    path("shop/<slug:slug>/", views.category_list, name="category_list"),
+    path("set_theme/", views.set_theme, name="set_theme"),
+
+]
